9feebcff642dbc454b14f80ac8be8ca0db6d4cd6
Akirapearl <draculaspot@hotmail.com>
2024-11-12 14:51:14 +0100

    Day 2 - Finished

--- day2/main.go
+++ day2/main.go
@@ -1,37 +1,5 @@
 package main
 
-/*
---- Day 2: Cube Conundrum ---
-
-As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue. Each time you play this game, he will hide a secret number
-of cubes of each color in the bag, and your goal is to figure out information about the number of cubes.
-
-To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you,
-and then put them back in the bag. He'll do this a few times per game.
-
-You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the 11 in Game 11: ...)
-followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
-
-For example, the record of a few games might look like this:
-
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-
-In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes and 4 red cubes;
-the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
-
-The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-
-In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration.
-However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been
-impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
-
-Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
-What is the sum of the IDs of those games?
-*/
 import (
 	"bufio"
 	"fmt"
@@ -41,11 +9,10 @@ import (
 	"strings"
 )
 
-func check(err error) error {
+func check(err error) {
 	if err != nil {
-		return err
+		panic(err)
 	}
-	return nil
 }
 
 type Colors struct {
@@ -65,10 +32,10 @@ func Sum(slice []int) int {
 func main() {
 	fmt.Println("Day 2")
 
-	path := "test.txt"
+	// Replace with your path to the actual input file
+	path := "input.txt"
 	// Task 1 - Get the numbers
 	file, err := os.Open(path)
-
 	if err != nil {
 		check(err)
 	}
@@ -76,7 +43,6 @@ func main() {
 
 	// Set values for colors
 	var TotalColors Colors
-
 	TotalColors.red = 12
 	TotalColors.green = 13
 	TotalColors.blue = 14
@@ -84,59 +50,53 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
 
-	//count := 0
-
+	var validGameIDs []int
 	for scanner.Scan() {
 		x := scanner.Text()
 
-		// Split by : to get each subset
+		// Split by ": " to get each subset
 		parts := strings.Split(x, ": ")
 		gameIDPart := parts[0]
 		subsetsPart := parts[1] // e.g., "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
 
 		// Extract the game ID from "Game X"
 		gameID := strings.TrimSpace(strings.TrimPrefix(gameIDPart, "Game "))
-		fmt.Println("GAME ======= ", gameID)
+		//fmt.Println("GAME ======= ", gameID)
 		//fmt.Println(subsetsPart)
 
 		// "Does at any point the elf show you more than 12 red, 13 green, or 14 blue cubes at one time? If not, the game is valid."
 
-		// Get the amount - colour pair via regex
+		// Get the amount-color pair via regex
 		re := regexp.MustCompile(`(\d+) (\w+)`)
 		// Find all matches
 		matches := re.FindAllStringSubmatch(subsetsPart, -1)
 
-		myInt := []int{}
-		var isWin bool
-		//count := 0
+		isValid := true
 		for _, match := range matches {
-			actualcount, err := strconv.Atoi(match[1])
+			actualCount, err := strconv.Atoi(match[1])
 			check(err)
-			if !(actualcount >= TotalColors.red) && match[2] == "red" {
-				isWin = true
 
-			} else if !(actualcount >= TotalColors.green) && match[2] == "green" {
-				isWin = true
-
-			} else if !(actualcount >= TotalColors.blue) && match[2] == "blue" {
-				isWin = true
-
-			} else {
-				isWin = false
+			// If the actual count exceeds the threshold for any color, mark the game as invalid
+			if match[2] == "red" && actualCount > TotalColors.red {
+				isValid = false
+				break
+			} else if match[2] == "green" && actualCount > TotalColors.green {
+				isValid = false
+				break
+			} else if match[2] == "blue" && actualCount > TotalColors.blue {
+				isValid = false
 				break
 			}
 		}
-		//fmt.Println(isWin)
 
-		isGameID, err := strconv.Atoi(gameID)
-		check(err)
-		if isWin {
-			myInt = append(myInt, isGameID)
-			//fmt.Println(myInt)
+		// If the game is valid, add its ID to the list
+		if isValid {
+			gameIDInt, err := strconv.Atoi(gameID)
+			check(err)
+			validGameIDs = append(validGameIDs, gameIDInt)
 		}
-		fmt.Println(Sum(myInt))
-		//count = count + myInt[0]*10 + myInt[len(myInt)-1]
-		//fmt.Println(count)
 	}
 
+	// Output the sum of valid game IDs after processing all lines
+	fmt.Println(Sum(validGameIDs))
 }
